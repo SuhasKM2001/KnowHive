c63a6588b9cfc8dffddb734694ee528574393375
SuhasKM <suhasvarun1@gmail.com>
2023-11-09 13:35:49 +0530

    Created react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import StartPage from "./pages/StartPage";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route index element= {<StartPage />} />
+        <Route path="/start" element={<StartPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
