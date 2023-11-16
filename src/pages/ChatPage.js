63b5a9e81f21bfb93d0cb26645860493488e1e4d
SuhasKM <suhasvarun1@gmail.com>
2023-11-16 18:57:30 +0530

    Added logo to start page

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 function ChatPage() {
   return (
     <motion.div initial={{ scaleY: 0 }} animate={{ scaleY: 1 }} exit={{ scaleY: 0 }} transition={{ duration: 0.5 }}>
-      <div className="flex flex-col items-center justify-center h-screen bg-primary">
+      <div>
         {/* Start the code from here */}
         <h1 className="text-5xl font-bold underline">Chat Page</h1>
       </div>
--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { TypeAnimation } from "react-type-animation";
+import Logo from "../assests/knowhiveLogo.png";
 
 function StartPage() {
   const navigate = useNavigate();
@@ -17,74 +18,81 @@ function StartPage() {
     visible: {
       opacity: 2,
       transition: {
-        duration: 0.8, 
-        delay: 1,
+        duration: 0.8,
+        delay: 1.2,
       },
     },
   };
 
   return (
-    <motion.div
-      initial={{ scaleY: 0 }}
-      animate={{ scaleY: 1 }}
-      exit={{ scaleY: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <div className="flex flex-col items-center justify-center h-screen bg-primary">
-        <TypeAnimation
-          sequence={["Welcome to KnowHive Assistant", 1000]}
-          speed={60}
-          className="py-2 font-poppins text-3xl text-center"
-          cursor={false}
-        />
+      <motion.div
+        initial={{ scaleY: 0 }}
+        animate={{ scaleY: 1 }}
+        exit={{ scaleY: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="flex flex-col items-center justify-center h-screen bg-primary">
+          {/* <div className="absolute bottom-0 left-0 top-0 right-0 bg-primary"> */}
 
-        <motion.div
-          variants={textVariants}
-          initial="hidden"
-          animate="visible"
-          className="py-2 font-poppins text-sm text-center"
-        >
-          🚀Experience KnowHive in Computing
-        </motion.div>
+          <img src={Logo} alt="KnowHive Logo" width="200" height={200} />
 
-        <motion.div
-          variants={textVariants}
-          initial="hidden"
-          animate="visible"
-          className="py-2 font-poppins text-sm text-center"
-        >
-          🔮Find the solutions seamlessly
-        </motion.div>
+          <TypeAnimation
+            sequence={["Welcome to KnowHive Assistant", 1000]}
+            speed={50}
+            className="py-2 font-poppins text-3xl text-center"
+            cursor={false}
+          />
 
-        <motion.div
-          variants={textVariants}
-          initial="hidden"
-          animate="visible"
-          className="py-2 font-poppins text-sm text-center"
-        >
-          🌐Elevate your Understanding
-        </motion.div>
-
-        <motion.div
-          variants={textVariants}
-          initial="hidden"
-          animate="visible"
-          className="py-2 font-poppins text-sm text-center"
-        >
-          ⚙️Simple Interface
-        </motion.div>
-
-        <motion.div variants={textVariants} initial="hidden" animate="visible">
-          <button
-            className="border font-poppins text-base bg-secondary text-white my-2 px-5 py-1 text-center rounded-full"
-            onClick={handleButtonClick}
+          <motion.div
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+            className="py-2 font-poppins text-sm text-center"
           >
-            Let's Start
-          </button>
-        </motion.div>
+            🚀Experience KnowHive in Computing
+          </motion.div>
 
-      </div>
-    </motion.div>
+          <motion.div
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+            className="py-2 font-poppins text-sm text-center"
+          >
+            🔮Find the solutions seamlessly
+          </motion.div>
+
+          <motion.div
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+            className="py-2 font-poppins text-sm text-center"
+          >
+            🌐Elevate your Understanding
+          </motion.div>
+
+          <motion.div
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+            className="py-2 font-poppins text-sm text-center"
+          >
+            ⚙️Simple Interface
+          </motion.div>
+
+          <motion.div
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+          >
+            <button
+              className="border font-poppins text-base bg-secondary text-white my-2 px-5 py-1 text-center rounded-full"
+              onClick={handleButtonClick}
+            >
+              Let's Start
+            </button>
+          </motion.div>
+        </div>
+      </motion.div>
   );
 }
 
